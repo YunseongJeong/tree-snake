2a886d4f4caf5a0fd8cc5e9d846ef19915da9ecd
mandu <jys0972@gmail.com>
2024-06-02 22:23:36 +0900

    update head
    change head moveable by key input
    and face to direction

---
+++ Assets/scripts/Head.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Head : Node
+{
+    private Vector3 direction = new Vector3();
+    private float speed = 5;
+
+    void Start()
+    {
+        base.Start();
+    }
+
+    void Update()
+    {
+        MoveByKey();
+    }
+
+    private void MoveByKey()
+    {
+        direction.Set(0, 0, 0);
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction.y += 1;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction.x -= 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction.y -= 1;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction.x += 1;
+        }
+        faceDirection(direction);
+        direction.Normalize();
+        
+        transform.position += direction * speed * Time.deltaTime;
+    }
+
+    private void faceDirection(Vector3 direction)
+    {
+        if (direction != Vector3.zero)
+        {
+            transform.eulerAngles = new Vector3(0, 0, Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg - 90);
+        }
+    }
+}
